981d0ba11f785d7f937d3f66ccaba3c0af000877
Einstein.dev <einstein.dev@hotmail.com>
2022-05-25 19:33:15 -0300

    feat(development): Estrutura MVCR preparada, com Front aplicado

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 
 const port = 3000
+app.set("view engine", "ejs");
+app.use(express.static('./Views'));
 
 //rota principal
 app.get('/',(req,res)=>{
-	res.send("Página Principal")
+	res.render("pages/index")
+})
+//rota pagina do controller
+app.get('/controller',(req,res)=>{
+	res.render("pages/paginaControle")
 })
 
 //conexão com servidor
